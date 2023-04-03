bd24ef9ffdec02250072267010ce18fa0ae62324
waesxl <kullq2@yandex.ru>
2023-04-03 16:46:52 +0300

    Object homework

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,10 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let count = 0;
+  for (let key in salaries) {
+    if (isFinite(salaries[key])) {
+      count += salaries[key];
+    }
+  }
+
+  return count;
 }
